5deaf0a2ea8b902f167a8152338e0240adb1c9bf
UdlaAriel <ariel.anchapaxi@gmail.com>
2025-05-24 15:33:38 -0500

    Modifying the logic for the RabbitMQProducer

--- RabbitMQTest/Infrastructure/RabbitMQ/RabbitMQProducer.cs
+++ RabbitMQTest/Infrastructure/RabbitMQ/RabbitMQProducer.cs
@@ -1,13 +1,23 @@
 using RabbitMQ.Client;
 using RabbitMQTest.Domain;
+using System.Text;
+using System.Threading.Channels;
 
 namespace RabbitMQTest.Infrastructure.RabbitMQ;
 
 public class RabbitMQProducer
 {
-    public static void SendAlert(AlertMessage alertMessage)
+    public async static void SendAlert(AlertMessage alertMessage)
     {
-        var message = new AlertMessage(alertMessage.Message, alertMessage.Binding);
+        var connection = await RabbitMQConnection.Instance!.Init();
 
+
+        await connection.Channel!.BasicPublishAsync(
+            exchange: string.Empty,
+            routingKey: alertMessage.Binding,
+            body: Encoding.UTF8.GetBytes(alertMessage.Message)
+            );
+
+        Console.WriteLine($" [x] Sent {alertMessage.Message}");
     }
 }
