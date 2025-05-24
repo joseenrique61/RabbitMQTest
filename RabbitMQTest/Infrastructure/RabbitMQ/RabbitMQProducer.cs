3cb0b3565db443c7ac16e71ea538d869ffee6753
UdlaAriel <ariel.anchapaxi@gmail.com>
2025-05-24 15:18:50 -0500

    Adding elements to .gitignore

--- RabbitMQTest/Infrastructure/RabbitMQ/RabbitMQProducer.cs
+++ RabbitMQTest/Infrastructure/RabbitMQ/RabbitMQProducer.cs
@@ -1,3 +1,4 @@
+using RabbitMQ.Client;
 using RabbitMQTest.Domain;
 
 namespace RabbitMQTest.Infrastructure.RabbitMQ;
@@ -6,6 +7,7 @@ public class RabbitMQProducer
 {
     public static void SendAlert(AlertMessage alertMessage)
     {
+        var message = new AlertMessage(alertMessage.Message, alertMessage.Binding);
 
     }
 }
