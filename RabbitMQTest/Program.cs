c127608210a5038477efe0fc772765add64a9529
UdlaAriel <ariel.anchapaxi@gmail.com>
2025-05-24 20:19:15 -0500

    Solving merge conflicts

---
+++ RabbitMQTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace RabbitMQTest;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder();
+
+
+
+        using var host = builder.Build();
+    }
+}
